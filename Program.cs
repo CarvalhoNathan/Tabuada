b35629c8d9d92a80cf188195d8c8cf26cc34fb67
CarvalhoNathan <fariasnathan75@gmail.com>
2025-06-02 16:13:44 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Receba um número e logo em seguida exiba sua tabuada.
+
+Console.Clear();
+
+Console.WriteLine("Digite um número para ver sua tabuada:");
+string entrada = Console.ReadLine()!;
+
+if (int.TryParse(entrada, out int numero))
+{
+    Console.Clear();
+    Console.WriteLine($"Tabuada do {numero}:\n");
+    for (int i = 1; i <= 10; i++)
+    {
+        int resultado = numero * i;
+        Console.WriteLine($"{numero} x {i} = {resultado}");
+    }
+}
+else
+{
+    Console.Clear();
+    Console.WriteLine("Por favor, digite um número válido!");
+}
